f32cee059597e7f83602b3e83fce8a1102b734e5
Alessandro <nasc007@gmail.com>
2022-01-03 11:51:01 -0300

    fim

--- jogo.js
+++ jogo.js
@@ -1,6 +1,20 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 15
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal') {
+	criaMosquitoTempo = 1500
+} else if (nivel === 'dificil') {
+	criaMosquitoTempo = 1000
+}else if (nivel === 'chucknorris') {
+	criaMosquitoTempo = 750
+}
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -11,10 +25,29 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(() => {
+	tempo -= 1
+	
+	if (tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosquito)
+		window.location.href = 'vitoria.html'
+	}else{
+		document.getElementById('tempo').innerHTML = tempo
+	}
+	
+}, 1000);
+
 function posicaoRandomica() {
 
 	if (document.getElementById('mosquito')) {
 		document.getElementById('mosquito').remove()
+
+		if (vidas > 3) {
+			window.location.href = 'fim_de_jogo.html'
+		}
+		document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+		vidas++
 	}
 
 	let posicaoX = Math.floor(Math.random() * largura) - 120
@@ -32,6 +65,9 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	mosquito.id = 'mosquito'
+	mosquito.onclick = function(){
+		this.remove()
+	}
 
 	document.body.appendChild(mosquito)
 
